303262bf85ec53d0487881d83967ca918380b5fa
ariel_braga <ariel.braga@qualirede.com.br>
2023-04-07 22:40:35 -0300

    Implementado dashboard do backend

--- SimpleCodeTube/backend/controllers/SiteController.php
+++ SimpleCodeTube/backend/controllers/SiteController.php
@@ -2,12 +2,14 @@
 
 namespace backend\controllers;
 
-use common\models\LoginForm;
+use common\models\Subscriber;
+use common\models\Video;
+use common\models\VideoView;
 use Yii;
+use yii\web\Controller;
 use yii\filters\VerbFilter;
 use yii\filters\AccessControl;
-use yii\web\Controller;
-use yii\web\Response;
+use common\models\LoginForm;
 
 /**
  * Site controller
@@ -21,7 +23,7 @@ class SiteController extends Controller
     {
         return [
             'access' => [
-                'class' => AccessControl::class,
+                'class' => AccessControl::className(),
                 'rules' => [
                     [
                         'actions' => ['login', 'error'],
@@ -35,7 +37,7 @@ class SiteController extends Controller
                 ],
             ],
             'verbs' => [
-                'class' => VerbFilter::class,
+                'class' => VerbFilter::className(),
                 'actions' => [
                     'logout' => ['post'],
                 ],
@@ -50,7 +52,7 @@ class SiteController extends Controller
     {
         return [
             'error' => [
-                'class' => \yii\web\ErrorAction::class,
+                'class' => 'yii\web\ErrorAction',
             ],
         ];
     }
@@ -62,38 +64,69 @@ class SiteController extends Controller
      */
     public function actionIndex()
     {
-        return $this->render('index');
+        $user = Yii::$app->user->identity;
+        $userId = $user->id;
+        $latestVideo = Video::find()
+            ->latest()
+            ->creator($userId)
+            ->limit(1)
+            ->one();
+        $numberOfView = VideoView::find()
+            ->alias('vv')
+            ->innerJoin(Video::tableName() . ' v',
+                'v.video_id = vv.video_id')
+            ->andWhere(['v.created_by' => $userId])
+            ->count();
+
+        $numberOfSubscribers = Yii::$app->cache->get('subscribers-'.$userId);
+        if (!$numberOfSubscribers) {
+            $numberOfSubscribers = $user->getSubscribers()->count();
+            Yii::$app->cache->set('subscribers-'.$userId, $numberOfSubscribers);
+        }
+
+        $subscribers = Subscriber::find()
+            ->with('user')
+            ->andWhere(['channel_id' => $userId])
+            ->orderBy('created_at DESC')
+            ->limit(3)
+            ->all();
+
+        return $this->render('index', [
+            'latestVideo' => $latestVideo,
+            'numberOfView' => $numberOfView,
+            'numberOfSubscribers' => $numberOfSubscribers,
+            'subscribers' => $subscribers,
+        ]);
     }
 
     /**
      * Login action.
      *
-     * @return string|Response
+     * @return string
      */
     public function actionLogin()
     {
+        $this->layout = 'auth';
         if (!Yii::$app->user->isGuest) {
             return $this->goHome();
         }
 
-        $this->layout = 'auth';
-
         $model = new LoginForm();
         if ($model->load(Yii::$app->request->post()) && $model->login()) {
             return $this->goBack();
+        } else {
+            $model->password = '';
+
+            return $this->render('login', [
+                'model' => $model,
+            ]);
         }
-
-        $model->password = '';
-
-        return $this->render('login', [
-            'model' => $model,
-        ]);
     }
 
     /**
      * Logout action.
      *
-     * @return Response
+     * @return string
      */
     public function actionLogout()
     {
